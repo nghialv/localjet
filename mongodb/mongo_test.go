692724d36158b26ad72f69932733652c49a1f59a
nghialv <nghialv2607@gmail.com>
2017-04-21 11:07:46 +0900

    test

--- mongodb/mongo_test.go
+++ mongodb/mongo_test.go
@@ -2,11 +2,13 @@ package mongodb
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestInit(t *testing.T) {
 	err := initClient()
+	time.Sleep(1 * time.Minute)
 	assert.NoError(t, err)
 }
